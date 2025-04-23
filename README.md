6b126f3066d1e49217437682b392aca54cf0c26f
Matthias Telöken <matthias.teloeken@gmail.com>
2025-04-24 00:27:04 +0200

    reburfishing
    useing enums and newtype for more clarity,
    use deque, omit deepcopy,
    avoid unneccessary string conversions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SwitchKnights
+
+Refurbished 'lil programming puzzle back from the student days.
+
+> Objective: Switch the positions of black and white knights on a 9x9 chessboard.
--- src/switchknights/SwitchKnights.py
+++ src/switchknights/SwitchKnights.py
@@ -1,66 +1,76 @@
-import queue
 import time
-from copy import deepcopy
+from collections import deque
+from enum import IntEnum, unique
+from typing import NamedTuple, NewType
+
+Coord = NewType("Coord", int)
 
 
-class Position:
-    """Position Class."""
+@unique
+class Knight(IntEnum):
+    """Knight piece."""
 
-    __slots__ = ("k1", "k2", "k3", "k4", "position_list")
+    WHITE_1 = 0
+    WHITE_2 = 1
+    BLACK_1 = 2
+    BLACK_2 = 3
 
-    def __init__(self, knight1: int, knight2: int, knight3: int, knight4: int) -> None:
+
+class KnightPosition(NamedTuple):
+    """Position class."""
+
+    white_knight_1: Coord
+    white_knight_2: Coord
+    black_knight_1: Coord
+    black_knight_2: Coord
+
+    def __str__(self) -> str:
+        """To string of Position class."""
+        return f"{self.white_knight_1}{self.white_knight_2}{self.black_knight_1}{self.black_knight_2}"
+
+
+class TurnOrder:
+    """TurnOrder Class."""
+
+    __slots__ = ("position", "position_list")
+
+    def __init__(
+        self,
+        white_knight_1: Coord,
+        white_knight_2: Coord,
+        black_knight_1: Coord,
+        black_knight_2: Coord,
+    ) -> None:
         """Initialize Position Object."""
-        self.k1 = knight1
-        self.k2 = knight2
-        self.k3 = knight3
-        self.k4 = knight4
-        self.position_list = []
-        self.position_list.append((self.k1, self.k2, self.k3, self.k4))
+        self.position = KnightPosition(
+            white_knight_1, white_knight_2, black_knight_1, black_knight_2
+        )
+        self.position_list = [
+            self.position,
+        ]
 
-    def get_coord(self, piece_number: int) -> int:
+    def get_coord(self, knight: Knight) -> Coord:
         """Get coordinate of the knight piece."""
-        match piece_number:
-            case 0:
-                return self.k1
-            case 1:
-                return self.k2
-            case 2:
-                return self.k3
-            case 3:
-                return self.k4
-        return -1
-
-    def set_coord(self, piece_number: int, new_coord: int) -> None:
-        """Set the coordinate for the knight piece."""
-        match piece_number:
-            case 0:
-                self.k1 = new_coord
-            case 1:
-                self.k2 = new_coord
-            case 2:
-                self.k3 = new_coord
-            case 3:
-                self.k4 = new_coord
-        self.position_list.append((self.k1, self.k2, self.k3, self.k4))
+        return self.position[knight.value]
 
     def print_trace(self) -> None:
         """Print the solution."""
         for pos in self.position_list:
             print(pos)
-            print(Position.decode_pos_t(pos))
+            print(TurnOrder.decode_pos_t(pos))
 
     def move_count(self) -> int:
         """Get move count."""
         return len(self.position_list) - 1
 
     @staticmethod
-    def decode_pos_t(pos_t: tuple[int, int, int, int]) -> str:
+    def decode_pos_t(pos_t: KnightPosition) -> str:
         """Decode position."""
         field = ["." for _ in range(9)]
-        field[pos_t[0]] = "W"
-        field[pos_t[1]] = "W"
-        field[pos_t[2]] = "B"
-        field[pos_t[3]] = "B"
+        field[pos_t[Knight.WHITE_1.value]] = "W"
+        field[pos_t[Knight.WHITE_2.value]] = "W"
+        field[pos_t[Knight.BLACK_1.value]] = "B"
+        field[pos_t[Knight.BLACK_2.value]] = "B"
         output_str = ""
         for i in range(9):
             output_str = output_str + field[i] + " "
@@ -70,91 +80,105 @@ class Position:
 
     def __str__(self) -> str:
         """To string of Position class."""
-        if self.k1 > self.k2:
-            output_str = str(self.k2) + str(self.k1)
-        else:
-            output_str = str(self.k1) + str(self.k2)
-        if self.k3 > self.k4:
-            output_str = output_str + str(self.k4) + str(self.k3)
-        else:
-            output_str = output_str + str(self.k3) + str(self.k4)
-        return output_str
+        return self.position.__str__()
 
 
 class Chessboard:
-    """Chessboard class."""
+    """Chessboard class.
 
-    def __init__(self) -> None:
-        """Initialize Chessboard."""
-        self.possible_moves = []
-        self.possible_moves.append([1, 2])
-        self.possible_moves.append([2, 3])
-        self.possible_moves.append([3, 4])
-        self.possible_moves.append([1, 8])
-        self.possible_moves.append([0])
-        self.possible_moves.append([4, 5])
-        self.possible_moves.append([7, 8])
-        self.possible_moves.append([6, 7])
-        self.possible_moves.append([5, 6])
+    Coordinate system:
+    0 1 2
+    3 4 5
+    6 7 8
+    """
 
-    def get_moves(self, coord: int) -> list[int]:
-        """Get moves for coordinate."""
-        return self.possible_moves[coord]
+    class MoveVariation(IntEnum):
+        """Move variation Enum.
+
+        U = Up
+        D = Down
+        L = Left
+        R = Right
+        """
+
+        UUL = -7
+        UUR = -5
+        URR = -1
+        DRR = 5
+        DDR = 7
+        DDL = 5
+        DLL = 1
+        ULL = -5
+
+    possible_moves: dict[Coord, list[MoveVariation]] = {
+        Coord(0): [MoveVariation.DDR, MoveVariation.DRR],
+        Coord(1): [MoveVariation.DDL, MoveVariation.DDR],
+        Coord(2): [MoveVariation.DDL, MoveVariation.DLL],
+        Coord(3): [MoveVariation.URR, MoveVariation.DRR],
+        Coord(4): [],
+        Coord(5): [MoveVariation.ULL, MoveVariation.DLL],
+        Coord(6): [MoveVariation.UUR, MoveVariation.URR],
+        Coord(7): [MoveVariation.UUL, MoveVariation.UUR],
+        Coord(8): [MoveVariation.UUL, MoveVariation.ULL],
+    }
 
     @staticmethod
-    def knights_move(variation: int, coord: int) -> int:
-        """Knights move."""
-        match variation:
-            case 1 | 3:
-                return coord + 5
-            case 2:
-                return coord + 7
-            case 4:
-                return coord + 1
-            case 5 | 7:
-                return coord - 5
-            case 6:
-                return coord - 7
-            case 8:
-                return coord - 1
-        return -1
+    def get_moves(coord: Coord) -> list[Coord]:
+        """Get moves for coordinate."""
+        return [
+            Coord(coord + possible_move)
+            for possible_move in Chessboard.possible_moves[coord]
+        ]
 
 
 class SwapKnights:
     """SwapKnights class."""
 
+    target_positions = {
+        KnightPosition(Coord(6), Coord(8), Coord(0), Coord(2)),
+        KnightPosition(Coord(8), Coord(6), Coord(0), Coord(2)),
+        KnightPosition(Coord(6), Coord(8), Coord(2), Coord(0)),
+        KnightPosition(Coord(8), Coord(6), Coord(2), Coord(0)),
+    }
+
     def __init__(self) -> None:
         """Initialize SwapKnights."""
-        self.processed_positions: set[str] = set()
-        self.board: Chessboard = Chessboard()
-        self.solve()
+        self.processed_positions: set[KnightPosition] = set()
+        self.win_condition: KnightPosition = KnightPosition(
+            Coord(0), Coord(2), Coord(6), Coord(8)
+        )
+        if (solution := self.solve()) is not None:
+            print(f"Solution found: {solution.move_count()} moves")
+            solution.print_trace()
+        else:
+            print("No solution found")
 
-    def solve(self) -> None:
+    def solve(self) -> TurnOrder | None:
         """Solve the knights puzzle."""
-        positions = queue.Queue()
-        positions.put(Position(0, 2, 6, 8))
-        while not positions.empty():
-            pos = positions.get()
-            if (pos_str := str(pos)) == "6802" or pos_str == '8602' or pos_str == '6820' or pos_str == '8620':
-                print(f"Solution found: {pos.move_count()} moves")
-                pos.print_trace()
-                break
-            elif str(pos) in self.processed_positions:
+        positions = deque[TurnOrder]()
+        positions.append(TurnOrder(Coord(0), Coord(2), Coord(6), Coord(8)))
+        while len(positions) > 0:
+            pos: TurnOrder = positions.popleft()
+            if pos.position in SwapKnights.target_positions:
+                return pos
+            elif pos.position in self.processed_positions:
                 continue
             else:
-                self.processed_positions.add(str(pos))
+                self.processed_positions.add(pos.position)
                 # move 4 pieces
-                for piece in range(4):
+                for piece in list(Knight):
                     coord = pos.get_coord(piece)
-                    possible_moves = self.board.get_moves(coord)
-                    for possible_move in possible_moves:
-                        new_coord = Chessboard.knights_move(
-                            possible_move, pos.get_coord(piece)
-                        )
-                        if str(pos).count(str(new_coord)) == 0:
-                            new_pos = deepcopy(pos)
-                            new_pos.set_coord(piece, new_coord)
-                            positions.put(new_pos)
+                    for new_coord in Chessboard.get_moves(coord):
+                        # check if the new position is not occupied by another knight
+                        if pos.position.count(new_coord) == 0:
+                            new_position_values = list(pos.position)
+                            new_position_values[piece.value] = new_coord
+                            new_pos = TurnOrder(*new_position_values)
+                            new_pos.position_list = pos.position_list + [
+                                new_pos.position
+                            ]
+                            positions.append(new_pos)
+        return None
 
 
 if __name__ == "__main__":
